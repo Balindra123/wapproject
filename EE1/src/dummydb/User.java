f878ee4f079f79d88c2606351c4449dd0f0381bc
balindrarayamajhi <balindrarayamajhi@172.19.144.224>
2018-11-16 18:21:14 -0600

    test commit

---
+++ EE1/src/dummydb/User.java
@@ -0,0 +1,28 @@
+package dummydb;
+
+
+public class User {
+	private String name;
+	private String email;
+	private String password;
+	
+	User(String name, String email, String password) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+}
